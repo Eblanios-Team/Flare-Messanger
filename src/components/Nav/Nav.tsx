a37e2fa99bdb9b50cbbd93052ee698e0ea5b17e9
Andrew <andrewlokoffical@gmail.com>
2025-02-26 23:45:12 +0100

    Styled users in the Contacts component

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,28 +4,28 @@ export default function Nav() {
 
   return (
     <nav className="h-screen">
-      <div className="w-[175px] bg-orange h-full hidden  flex flex-col items-center sm:flex flex-col items-center">
+      <div className="w-[100px] bg-orange h-full hidden  flex flex-col items-center sm:flex flex-col items-center">
         {/* Аватар */}
         <button className="mt-[60px] mb-[60px]">
-          <img src="/img/avatar.png" alt="Avatar" className="border-solid border-2 border-white rounded-[100px]"/>
+          <img src="/img/avatar.png" alt="Avatar" className="border-solid border-2 border-white rounded-[100px] w-[60px]"/>
         </button>
 
         {/* Список навигации */}
         <ul className="flex flex-col items-center flex-grow w-full">
-          <li className="mb-[48px]">
-            <a href="" className=""><img src="/img/chat.png" alt="Chat" /></a>
+          <li className="mb-[30px]">
+            <a href="" className=""><img src="/img/chat.png" alt="Chat" className="w-[40px]" /></a>
           </li>
-          <li className="mb-[48px]">
-            <a href=""><img src="/img/notification.png" alt="Notifications" /></a>
+          <li className="mb-[30px]">
+            <a href=""><img src="/img/notification.png" alt="Notifications" className="w-[40px]" /></a>
           </li>
           <li>
-            <button><img src="/img/settings.png" alt="Settings" /></button>
+            <button><img src="/img/settings.png" alt="Settings" className="w-[40px]" /></button>
           </li>
         </ul>
 
         {/* Кнопка выхода */}
         <Link to={'/login'} className="p-4 mb-5">
-          <img src="/img/exit.png" alt="Exit" />
+          <img src="/img/exit.png" alt="Exit" className="w-[40px]" />
         </Link>
       </div>
     </nav>
--- src/components/Nav/NavMobile.tsx
+++ src/components/Nav/NavMobile.tsx
@@ -8,38 +8,38 @@ const NavMobile = ({ isOpen, toggleMenu }: { isOpen: boolean; toggleMenu: () => 
       {/* Затемнение фона */}
       <div className="absolute inset-0 bg-black bg-opacity-50" onClick={toggleMenu}></div>
 
-      <nav className="w-[175px] bg-orange h-full flex flex-col items-center relative z-50">
+      <nav className="w-[100px] bg-orange h-full flex flex-col items-center relative z-50">
         {/* Аватар */}
         <button className="mt-[60px] mb-[60px]">
           <img
             src="/img/avatar.png"
             alt="Avatar"
-            className="border-solid border-2 border-white rounded-[100px]"
+            className="border-solid border-2 border-white rounded-[100px] w-[60px]"
           />
         </button>
 
         {/* Список навигации */}
         <ul className="flex flex-col items-center flex-grow w-full">
-          <li className="mb-[48px]">
+          <li className="mb-[30px]">
             <a href="">
-              <img src="/img/chat.png" alt="Chat" />
+              <img src="/img/chat.png" alt="Chat" className="w-[40px]"/>
             </a>
           </li>
-          <li className="mb-[48px]">
+          <li className="mb-[30px]">
             <a href="">
-              <img src="/img/notification.png" alt="Notifications" />
+              <img src="/img/notification.png" alt="Notifications" className="w-[40px]" />
             </a>
           </li>
           <li>
             <button>
-              <img src="/img/settings.png" alt="Settings" />
+              <img src="/img/settings.png" alt="Settings" className="w-[40px]" />
             </button>
           </li>
         </ul>
 
         {/* Кнопка выхода */}
         <Link to={"/login"} className="p-4 mb-5">
-          <img src="/img/exit.png" alt="Exit" />
+          <img src="/img/exit.png" alt="Exit" className="w-[40px]" />
         </Link>
       </nav>
     </div>
